a5505ca41d37fbd060c06ee56da8deefa08a0717
riko <mhmmdrko@student.telkomuniversity.ac.id>
2021-05-04 11:54:36 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4052/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if4052/galerihewan/model/Hewan.kt
@@ -1,7 +1,9 @@
 package org.d3if4052.galerihewan.model
 
+import org.d3if4052.galerihewan.R
+
 data class Hewan (
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int
+    val imageResId: Int = R.drawable.angsa
 )
